ff63a18284c76b79c95617814854d102aa508f1e
NathanTamiru <mr53017@umbc.edu>
2022-06-30 09:00:46 -0400

    uploads setup

--- frontend/src/Uploads.js
+++ frontend/src/Uploads.js
@@ -1,1 +1,17 @@
 
+import './App.css';
+import {Route,Routes} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+
+function Uploads() {
+  return (
+   
+<div></div>
+  );
+
+
+}
+
+
+export default Uploads;
