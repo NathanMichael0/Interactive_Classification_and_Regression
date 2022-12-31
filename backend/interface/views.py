0e35900237bc2730d88f16c5a3362f25620bc8fe
Nathan <nathan@Nathans-Laptop.lan1>
2022-12-31 00:50:36 -0500

    requirements.txt

--- backend/interface/views.py
+++ backend/interface/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect  
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from flask import jsonify
 import json
@@ -21,215 +21,182 @@ from sklearn.svm import SVC
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import LinearRegression
 
+# Create your views here.
 
 
+def reg(request):
+    targetClass = json.loads(request.body)["tgtClass"]
+    newCsv = pd.DataFrame(json.loads(request.body)["newJsonCsv"])
+    testSize = json.loads(request.body)["ttestsize"]
+    parameterString = json.loads(request.body)["parameters"]
+    userChoice = json.loads(request.body)["classReg"]
+    featureImp = []
+    rocArr = []
+
+    print(newCsv.head())
+
+    X = newCsv.drop(targetClass, axis=1)
+    y = newCsv[targetClass]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=testSize)
+    if (userChoice == "linreg"):
+        reg_clf = eval(
+            "LinearRegression("+parameterString[0:len(parameterString) - 1]+")")
+        reg_clf.fit(X_train, y_train)
+
+        rscore = reg_clf.score(X_train, y_train)
+
+        i = 0
+        for name in (reg_clf.feature_names_in_):
+            featureImp.append({"name": name, "coef": (reg_clf.coef_)[i]})
+            i += 1
+
+    elif (userChoice == "logreg"):
+        reg_clf = eval(
+            "LogisticRegression("+parameterString[0:len(parameterString) - 1]+")")
+
+        reg_clf.fit(X_train, y_train)
+
+        rscore = reg_clf.score(X_train, y_train)
+
+        i = 0
+        for name in (reg_clf.feature_names_in_):
+            featureImp.append({"name": name, "coef": (reg_clf.coef_[0])[i]})
+            i += 1
+
+    elif (userChoice == "ridgereg"):
+        reg_clf = eval(
+            "Ridge("+parameterString[0:len(parameterString) - 1]+")")
+
+        reg_clf.fit(X_train, y_train)
+
+        rscore = reg_clf.score(X_train, y_train)
+        i = 0
+        for name in (reg_clf.feature_names_in_):
+            featureImp.append({"name": name, "coef": (reg_clf.coef_)[i]})
+            i += 1
+
+    return JsonResponse({"rscore": rscore,  "featureImp": featureImp, })
 
 
-    # Create your views here.  
-def reg(request):  
-        targetClass = json.loads(request.body)["tgtClass"]
-        newCsv = pd.DataFrame(json.loads(request.body)["newJsonCsv"])
-        testSize = json.loads(request.body)["ttestsize"]
-        parameterString =json.loads(request.body)["parameters"]
-        userChoice = json.loads(request.body)["classReg"]
-        featureImp = []
-        rocArr = []
+def clf(request):
+    # if request.method == "POST":
+    # print("showing req",json.loads(request.body))
 
-        print(newCsv.head())
+    targetClass = json.loads(request.body)["tgtClass"]
+    newCsv = pd.DataFrame(json.loads(request.body)["newJsonCsv"])
+    testSize = json.loads(request.body)["ttestsize"]
+    parameterString = json.loads(request.body)["parameters"]
+    userChoice = json.loads(request.body)["classReg"]
+    featureImp = []
+    rocArr = []
 
-        X =  newCsv.drop(targetClass, axis=1)
-        y = newCsv[targetClass]
+    print(newCsv.head())
 
-  
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize)
-        if (userChoice == "linreg"):
-            reg_clf =  eval("LinearRegression("+parameterString[0:len(parameterString) -1]+")") 
-            reg_clf.fit(X_train,y_train)
+    X = newCsv.drop(targetClass, axis=1)
+    y = newCsv[targetClass]
 
-            rscore = reg_clf.score(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=testSize)
 
-            i=0          
-            for name in (reg_clf.feature_names_in_):
-                featureImp.append({"name":name,"coef":(reg_clf.coef_)[i]})
-                i+=1
-            
+    if (userChoice == "logreg"):
+        reg_clf = eval(
+            "LogisticRegression("+parameterString[0:len(parameterString) - 1]+")")
+        reg_clf.fit(X_train, y_train)
+        i = 0
+        for name in (reg_clf.feature_names_in_):
+            featureImp.append({"name": name, "coef": (reg_clf.coef_)[0][i]})
+            i += 1
 
+        logreg_probs = reg_clf.predict_proba(X_test)
+        logreg_preds = logreg_probs[:, 1]
+        logreg_fpr, logreg_tpr, logreg_threshold = metrics.roc_curve(
+            y_test, logreg_preds)
+        auc = metrics.auc(logreg_fpr, logreg_tpr)
 
-        elif (userChoice == "logreg"):
-             reg_clf =  eval("LogisticRegression("+parameterString[0:len(parameterString) -1]+")")  
-            
-             reg_clf.fit(X_train,y_train)
+        i = 0
+        for j in (list(logreg_tpr)):
+            rocArr.append({"tpr": j, "fpr": (list(logreg_fpr))[i]})
+            i += 1
 
-             rscore = reg_clf.score(X_train, y_train)
+    elif (userChoice == "linsvc"):
+        reg_clf = eval(
+            "LinearSVC("+parameterString[0:len(parameterString) - 1]+")")
+        reg_clf.fit(X_train, y_train)
+        i = 0
+        for name in (reg_clf.feature_names_in_):
+            featureImp.append({"name": name, "coef": (reg_clf.coef_)[0][i]})
+            i += 1
 
+        svm_probs = reg_clf.decision_function(X_test)
+        svm_preds = svm_probs
+        svm_fpr, svm_tpr, svm_threshold = metrics.roc_curve(y_test, svm_preds)
+        auc = metrics.auc(svm_fpr, svm_tpr)
 
-             i=0          
-             for name in (reg_clf.feature_names_in_):
-                featureImp.append({"name":name,"coef":(reg_clf.coef_[0]) [i]})
-                i+=1
+        i = 0
+        for j in (list(svm_tpr)):
+            rocArr.append({"tpr": j, "fpr": (list(svm_fpr))[i]})
+            i += 1
 
+    elif (userChoice == "rand"):
+        reg_clf = eval("RandomForestClassifier(" +
+                       parameterString[0:len(parameterString) - 1]+")")
+        reg_clf.fit(X_train, y_train)
 
-        elif (userChoice == "ridgereg"):
-             reg_clf =  eval("Ridge("+parameterString[0:len(parameterString) -1]+")") 
-            
-             reg_clf.fit(X_train,y_train)
+        for name, score in zip(newCsv, reg_clf.feature_importances_):
+            featureImp.append({"name": name, "score": score})
+        rf_probs = reg_clf.predict_proba(X_test)
+        rf_preds = rf_probs[:, 1]
+        rf_fpr, rf_tpr, rf_threshold = metrics.roc_curve(y_test, rf_preds)
+        auc = metrics.auc(rf_fpr, rf_tpr)
 
-             rscore = reg_clf.score(X_train, y_train)
-             i=0          
-             for name in (reg_clf.feature_names_in_):
-                featureImp.append({"name":name,"coef":(reg_clf.coef_)[i]})
-                i+=1
- 
-        
+        i = 0
+        for j in (list(rf_tpr)):
+            rocArr.append({"tpr": j, "fpr": (list(rf_fpr))[i]})
+            i += 1
 
+        # rocArr= [{"tpr": list(rf_tpr), "fpr":list(rf_fpr), "rocauc":rf_roc_auc}]
 
-        return JsonResponse({"rscore":rscore,  "featureImp": featureImp,})
-
-def clf(request):  
-        #if request.method == "POST":  
-        #print("showing req",json.loads(request.body))
-
-        targetClass = json.loads(request.body)["tgtClass"]
-        newCsv = pd.DataFrame(json.loads(request.body)["newJsonCsv"])
-        testSize = json.loads(request.body)["ttestsize"]
-        parameterString =json.loads(request.body)["parameters"]
-        userChoice = json.loads(request.body)["classReg"]
-        featureImp = []
-        rocArr = []
-
-        print(newCsv.head())
-
-        X =  newCsv.drop(targetClass, axis=1)
-        y = newCsv[targetClass]
-
-  
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize)
-
-
-        if (userChoice == "logreg"):
-            reg_clf =  eval("LogisticRegression("+parameterString[0:len(parameterString) -1]+")") 
-            reg_clf.fit(X_train, y_train)
-            i=0
-            for name in (reg_clf.feature_names_in_):
-                featureImp.append({"name":name,"coef":(reg_clf.coef_)[0][i]})
-                i+=1
-
-            logreg_probs = reg_clf.predict_proba(X_test)
-            logreg_preds = logreg_probs[:,1]
-            logreg_fpr, logreg_tpr, logreg_threshold = metrics.roc_curve(y_test, logreg_preds)
-            auc = metrics.auc(logreg_fpr, logreg_tpr)
-
-            i=0          
-            for j in (list(logreg_tpr)):
-                rocArr.append({"tpr":j,"fpr":(list(logreg_fpr))[i]})
-                i+=1
+   # logreg_clf = LogisticRegression(solver='liblinear')
 
 
-        elif (userChoice == "linsvc"):
-             reg_clf =  eval("LinearSVC("+parameterString[0:len(parameterString) -1]+")")  
-             reg_clf.fit(X_train, y_train)  
-             i=0          
-             for name in (reg_clf.feature_names_in_):
-                featureImp.append({"name":name,"coef":(reg_clf.coef_)[0][i]})
-                i+=1
+# 10-fold cross validation with scoring='roc_auc'
+#  validation done on the whole X
+    cv_score = cross_val_score(
+        reg_clf, X, np.ravel(y), cv=10, scoring='roc_auc')
 
-             svm_probs = reg_clf.decision_function(X_test)
-             svm_preds = svm_probs
-             svm_fpr, svm_tpr, svm_threshold = metrics.roc_curve(y_test, svm_preds)
-             auc = metrics.auc(svm_fpr, svm_tpr)   
+    print("Mean CV Score ", cv_score.mean())
+
+    return JsonResponse({"meanCvScore": (cv_score.mean()), "featureImp": featureImp, "roc": rocArr, "auc": auc})
 
 
-             i=0          
-             for j in (list(svm_tpr)):
-                rocArr.append({"tpr":j,"fpr":(list(svm_fpr))[i]})
-                i+=1   
-
-
-
-        elif (userChoice == "rand"):
-             reg_clf =  eval("RandomForestClassifier("+parameterString[0:len(parameterString) -1]+")") 
-             reg_clf.fit(X_train, y_train)
-             
-        
-             for name, score in zip(newCsv, reg_clf.feature_importances_):
-                featureImp.append({"name":name,"score":score})
-             rf_probs = reg_clf.predict_proba(X_test)
-             rf_preds = rf_probs[:,1]
-             rf_fpr, rf_tpr, rf_threshold = metrics.roc_curve(y_test, rf_preds)
-             auc = metrics.auc(rf_fpr, rf_tpr)  
-              
-
-
-             i=0          
-             for j in (list(rf_tpr)):
-                rocArr.append({"tpr":j,"fpr":(list(rf_fpr))[i]})
-                i+=1
-
-
-
-
-
-             
-             #rocArr= [{"tpr": list(rf_tpr), "fpr":list(rf_fpr), "rocauc":rf_roc_auc}]
-
-
-  
-
-       #logreg_clf = LogisticRegression(solver='liblinear')
-
-        
-
-# Perform a 10-fold cross validation with scoring='roc_auc'
-# Note: cross validation should be done on the whole X
-        cv_score = cross_val_score(reg_clf, X,np.ravel(y), cv=10, scoring='roc_auc')
-
-
-        print("Mean CV Score ", cv_score.mean())  
-       
-            
-
-        return JsonResponse({"meanCvScore":(cv_score.mean()), "featureImp": featureImp, "roc": rocArr, "auc":auc})
-
-
-
-
-
-def edit(request):  
-    #print("showing req",(request.body))
+def edit(request):
+    # print("showing req",(request.body))
    # data = {}
     if "GET" == request.method:
-       return render(request, "uploadcsv.html",{})
+        return render(request, "uploadcsv.html", {})
 
-    #csv_file = request.FILES["csv_file"].temporary_file_path()
+    # csv_file = request.FILES["csv_file"].temporary_file_path()
     csv_file = (request.FILES)['files[]']
-    print("csv file here ",csv_file)
-    csv_data =  pd.read_csv(csv_file)
+    print("csv file here ", csv_file)
+    csv_data = pd.read_csv(csv_file)
     jsonArray = csv_data.to_json(orient="records")
 
-    
-  
-    #convert python jsonArray to JSON String and write to file
-    
+    # convert python jsonArray to JSON String and write to file
 
-	
-	
-    
-	
     print(csv_data.head())
 
-  
-    retData = {"csvdata":(json.loads(jsonArray))};
+    retData = {"csvdata": (json.loads(jsonArray))}
 
-  
-    return JsonResponse (retData)
+    return JsonResponse(retData)
 
-   
 
-        
-         
-def update(request, id):  
+def update(request, id):
 
-        return render(request, 'edit.html', {'employee': "employee"})  
-def destroy(request, id):  
+    return render(request, 'edit.html', {'employee': "employee"})
 
-        return redirect("/show")  
+
+def destroy(request, id):
+
+    return redirect("/show")
--- frontend/src/Uploads.js
+++ frontend/src/Uploads.js
@@ -2,110 +2,110 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import axios from "axios";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const baseUrl = "http://localhost:8000";
 
-export let JSON_= [{"HEKKO": "gg"}];
+export let JSON_ = [{ "HEKKO": "gg" }];
 
 
 function Uploads() {
-    const navigate = useNavigate();
-    let columnDrop =[];
-    const [csvFiles, updateCsvFiles] = useState({"file": "jkhjgvc"});
-    const [csvJSON, updateCsvJSON] = useState([""]);
-    const [csvColumns, updateCsvColumns] = useState([""]);
+  const navigate = useNavigate();
+  let columnDrop = [];
+  const [csvFiles, updateCsvFiles] = useState({ "file": "jkhjgvc" });
+  const [csvJSON, updateCsvJSON] = useState([""]);
+  const [csvColumns, updateCsvColumns] = useState([""]);
 
 
 
-    const handleFile = (e) =>{
-        let files = e.target.files;
-       
-        updateCsvFiles({ "files":files });
-    
-    
-      }
+  const handleFile = (e) => {
+    let files = e.target.files;
 
-    const handleUpload =  async ( e ) =>{
- 
-        // console.log(formData)
-     
-        ////extremely important
-        e.preventDefault();
-          let files = csvFiles["files"];
-      let formData = new FormData();
-        formData.append("files", files);
-        console.log( "form data",csvFiles["files"]);
-     
-         try {
-           
-           const config = {
-             headers: {
-               'content-type': 'multipart/form-data',
-               //'enctype': 'multipart/form-data',
-              
-               
-             },
-           };
-     
-           const data = await axios.post(`${baseUrl}/edit`, 
-           csvFiles['files'],config
-           );
-    
-        updateCsvJSON(data.data.csvdata);
-     
-     
-           
-               let keysJSON = Object.keys(csvJSON[0]);
-               updateCsvColumns(keysJSON);
+    updateCsvFiles({ "files": files });
 
-               JSON_ = (data.data.csvdata).slice();
 
-               console.log(JSON_) 
-  
-               navigate("/Clean")
-         
-         } catch (err) {
-     
-           console.log(err);
+  }
 
-         }
-         
-       
-       
+  const handleUpload = async (e) => {
 
-         
-     
-       }
-  
+    // console.log(formData)
+
+    ////extremely important
+    e.preventDefault();
+    let files = csvFiles["files"];
+    let formData = new FormData();
+    formData.append("files", files);
+    console.log("form data", csvFiles["files"]);
+
+    try {
+
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data',
+          //'enctype': 'multipart/form-data',
+
+
+        },
+      };
+
+      const data = await axios.post(`${baseUrl}/edit`,
+        csvFiles['files'], config
+      );
+
+      updateCsvJSON(data.data.csvdata);
+
+
+
+      let keysJSON = Object.keys(csvJSON[0]);
+      updateCsvColumns(keysJSON);
+
+      JSON_ = (data.data.csvdata).slice();
+
+      console.log(JSON_)
+
+      navigate("/Clean")
+
+    } catch (err) {
+
+      console.log(err);
+
+    }
+
+
+
+
+
+
+  }
+
 
 
 
   return (
-   
-<div>
-    <form  method="POST"  enctype="multipart/form-data"> 
-   <div >
-       <label>File:
-         <input  type="file" name="csv_file" id="csv_file" required="True" class="form-control"  onChange={(e) => handleFile(e)} ></input>
+
+    <div>
+      <form method="POST" enctype="multipart/form-data">
+        <div >
+          <label>File:
+            <input type="file" name="csv_file" id="csv_file" required="True" class="form-control" onChange={(e) => handleFile(e)} ></input>
           </label>
-       <div >
-       </div>                    
-   </div>
-   <div >                    
-       <div >
-            <button onClick={(e) => {handleUpload(e);}}>Upload </button>
-       </div> 
-   </div>
+          <div >
+          </div>
+        </div>
+        <div >
+          <div >
+            <button onClick={(e) => { handleUpload(e); }}>Upload </button>
+          </div>
+        </div>
 
 
 
-    </form>
-    
-    
+      </form>
+
+
     </div>
 
-    
+
   );
 
 
